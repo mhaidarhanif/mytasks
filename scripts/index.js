c04ce06fc6fb68f286620529ef073c92f1b72466
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-19 14:34:10 +0700

    Put ids into multiple important elements for the app

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,7 @@
-console.log("MyTasks");
-
 (function () {
     // Display date & time
     const today = dayjs(new Date()).format('dddd, D MMMM YYYY H:m')
     $("#today").text(today)
-
 })();
+
+console.log("MyTasks");
