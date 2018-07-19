6ac64ad5aed00c8eb1349d9cb3c2a35e23cc46ae
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-19 15:27:31 +0700

    Implement & categorize data storage, seeder, DOM elements, read, add

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,48 @@
-(function () {
-    // Display date & time
-    const today = dayjs(new Date()).format('dddd, D MMMM YYYY H:m')
-    $("#today").text(today)
-})();
+// -----------------------------------------------------------------------------
+// MESSAGE
 
 console.log("MyTasks");
 
+// Display date & time
+const today = dayjs(new Date()).format('dddd, D MMMM YYYY H:mm')
+$("#today").text(today)
+
+// -----------------------------------------------------------------------------
+// DATA STORAGE
+
 let DATA = {
-    tasks: [{
-            text: "Action to to",
-            date: new Date(),
-            complete: false
-        },
-        {
-            text: "Action to to",
-            date: new Date(),
-            complete: true
-        },
-        {
-            text: "Action to to",
-            date: new Date(),
-            complete: false
-        }
-    ]
+    tasks: []
 }
+
+const getTasks = () => {
+    return DATA.tasks
+}
+
+const setTasks = (tasks) => {
+    DATA.tasks = tasks
+}
+
+// -----------------------------------------------------------------------------
+// DATA SEEDER
+
+const seed = (tasks) => {
+    DATA.tasks.push({
+        text: `Action to to`,
+        created: new Date(),
+        complete: false
+    }, {
+        text: `Action to to`,
+        created: new Date(),
+        complete: true
+    }, {
+        text: `Action to to`,
+        created: new Date(),
+        complete: false
+    })
+}
+
+// -----------------------------------------------------------------------------
+// DOM ELEMENTS
 
 const container = $("#container")
 const title = $("#title")
@@ -32,9 +51,23 @@ const nav = $("#nav")
 const app = $("#app")
 const tasks = $("#tasks")
 const add = $("#add")
-const addInput = $("#add-input")
 const addButton = $("#add-button")
+
+// -----------------------------------------------------------------------------
+// READ
+
+const read = () => {
+
+}
+
+// -----------------------------------------------------------------------------
+// ADD
 
 add.on("submit", (event) => {
     event.preventDefault()
+
+    const addInput = $("#add-input")
+    const addInputTodo = addInput.val()
 })
+
+// -----------------------------------------------------------------------------
